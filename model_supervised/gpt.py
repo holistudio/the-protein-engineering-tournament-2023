82a2d95004512585f9122363bbb006530d5d38aa
holistudio <daniel.bin.lu@gmail.com>
2025-10-25 19:48:02 -0400

    Update gpt.py
    - GPT config
    - cuda device if available

--- model_supervised/gpt.py
+++ model_supervised/gpt.py
@@ -1,12 +1,23 @@
 import torch
-from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
 from load_data import ProcessedDatasets
 
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu:0')
+
 train_ds = ProcessedDatasets().train
 test_ds = ProcessedDatasets().test
 vocab_size = ProcessedDatasets().vocab_size
+
+GPT_CONFIG = {
+    "vocab_size": vocab_size, # Vocabulary size
+    "context_length": train_ds.input_seq.shape[1], # Context length
+    "emb_dim": 768, # Embedding dimension
+    "n_heads": 12, # Number of attention heads
+    "n_layers": 12, # Number of layers
+    "drop_rate": 0.1, # Dropout rate
+    "qkv_bias": False # Query-Key-Value bias
+}
 
 torch.manual_seed(1337)
 
