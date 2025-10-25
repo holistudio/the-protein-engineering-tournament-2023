5247919ae170ea00e353cc13706cfdbfdd960223
holistudio <daniel.bin.lu@gmail.com>
2025-10-25 19:54:19 -0400

    Update gpt.py
    - moved dataloader code from gpt.py to train.py
    - added GPT model classes

--- model_supervised/gpt.py
+++ model_supervised/gpt.py
@@ -1,41 +1,152 @@
 import torch
-from torch.utils.data import DataLoader
+import torch.nn as nn
 
-from load_data import ProcessedDatasets
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        
+        assert (d_out % num_heads == 0), \
+            "d_out must be divisible by num_heads"
+        
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads # compute output size of each head
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu:0')
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
 
-train_ds = ProcessedDatasets().train
-test_ds = ProcessedDatasets().test
-vocab_size = ProcessedDatasets().vocab_size
+        self.dropout = nn.Dropout(dropout)
 
-GPT_CONFIG = {
-    "vocab_size": vocab_size, # Vocabulary size
-    "context_length": train_ds.input_seq.shape[1], # Context length
-    "emb_dim": 768, # Embedding dimension
-    "n_heads": 12, # Number of attention heads
-    "n_layers": 12, # Number of layers
-    "drop_rate": 0.1, # Dropout rate
-    "qkv_bias": False # Query-Key-Value bias
-}
+        self.register_buffer(
+            "mask",
+            torch.triu(torch.ones(context_length, context_length),
+            diagonal=1)
+        )
 
-torch.manual_seed(1337)
+        self.out_proj = nn.Linear(d_out, d_out) # linear layer combines head outputs
 
-train_loader = DataLoader(
-    dataset=train_ds,
-    batch_size=32,
-    shuffle=True,
-    drop_last=True,
-    num_workers=0
-)
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
 
-test_loader = DataLoader(
-    dataset=test_ds,
-    batch_size=32,
-    shuffle=False,
-    drop_last=True,
-    num_workers=0
-)
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
 
-batch = next(iter(train_loader))
-print(batch[0].shape, batch[1].shape, vocab_size)
+        keys       = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        values   = values.view(b, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        keys       = keys.transpose(1, 2) # (b, num_heads, num_tokens, head_dim)
+        queries = queries.transpose(1, 2)
+        values   = values.transpose(1, 2)
+
+        attn_scores = queries @ keys.transpose(2, 3)
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+
+        attn_weights = self.dropout(attn_weights)
+
+        context_vec = (attn_weights @ values).transpose(1, 2) # (b, num_tokens, n_heads, head_dim)
+
+        context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out) # combine heads
+
+        context_vec = self.out_proj(context_vec)
+        return context_vec
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) *
+            (x + 0.044715 * torch.pow(x, 3))
+        ))
+    
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]), # arbitrary multiply by factor of 4
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]),
+        )
+    def forward(self, x):
+        return self.layers(x)
+    
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5 # in case var is actually 0, add small eps to prevent division by 0
+
+        # in case model performance improves without layer norm, 
+        # scale and shift parameters will change significantly during training
+        self.scale = nn.Parameter(torch.ones(emb_dim)) 
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+
+        # unbiased=False to keep computations consistent with original GPT-2
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+    
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"],
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"]
+        )
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+    
+    def forward(self, x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        x = shortcut + x
+
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = shortcut + x
+        return x
+        
+
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+        self.trf_blocks = nn.Sequential(
+            *[TransformerBlock(cfg)
+            for _ in range(cfg["n_layers"])]
+        )
+        self.final_norm = LayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(
+            cfg["emb_dim"], cfg["vocab_size"], bias=False
+        )
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(
+            torch.arange(seq_len, device=in_idx.device)
+        )
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
